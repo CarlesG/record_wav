edadcd6b3f77fc6fa3f3626863b5b41f02becec4
CarlesG <carlesgallardollopis@gmail.com>
2024-07-14 20:56:30 +0200

    Adding the functionalities of wait certain time

--- record.py
+++ record.py
@@ -7,42 +7,43 @@ import datetime
 import time
 import sounddevice as sd
 import soundfile as sf
+import argparse
 
-with open('log.txt', 'a') as log_file:
+parser = argparse.ArgumentParser(
+                    prog = 'record.py',
+                    description = 'Script for record audio:')
+parser.add_argument('--path', required = False,  help = 'path to save', default = './')
+parser.add_argument('t_on', type = float, help = 'T on recording')
+parser.add_argument('t_off', type = float,  help = 'T off of pause')
+args = parser.parse_args()
+
+# %%-----INPUT VARIABLES--------
+path = args.path
+location = 'BCA'
+system_device = 'JETSON'
+extension = '.wav'
+fs = 44100 
+duration_on = args.t_on  
+duration_off = args.t_off 
+list_devices = sd.query_devices()
+#--------------------------------
+
+with open(path + 'log.txt', 'a') as log_file:
     start_time = datetime.datetime.now().strftime("[%Y%m%d_%H%M%S]")
     data_log = start_time + ": Starting program for recording"
     log_file.write(data_log + "\n")
     print(data_log)
 log_file.close()
 
-# %%-----INPUT VARIABLES-----
-location = 'BCA'
-system_device = 'JETSON'
-extension = '.wav'
-fs = 44100 # sample rate
-duration_on = 10  # duration of recording
-duration_off =  2
-list_devices = sd.query_devices()
-#-------------------------
-
-#print(list_devices)
-
-# %% Recording
-# Para saber que dispositivos tenemos disponibles de audio: 
-# python3 -m sounddevices
-
 while True:
-    date_start = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-    file_name = location + '_' + system_device + '_' + date_start + extension
-    #myrecording = sd.rec(int(duration_on * fs), samplerate=fs, channels=2)
-    date_end = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-    time.sleep(duration_off)
-    with open('log.txt', 'a') as log_file:
+    with open(path + 'log.txt', 'a') as log_file:
+        date_start = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+        file_name = location + '_' + system_device + '_' + date_start + extension
         data_recorded = sd.rec(int(duration_on * fs), fs, channels = 1, blocking = True)
-        sf.write(file_name, data_recorded, fs)
-        data_log = "   - Start time: " + date_start + " -- " + "End time: " + date_end
+        date_end = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+        sf.write(path + file_name, data_recorded, fs)
+        data_log = "   + Start time: " + date_start + " -- " + "End time: " + date_end
         log_file.write(data_log + "\n")     
-        print("   - Start time: " + date_start + " -- " + "End time: " + date_end)
+        print("   + Start time: " + date_start + " -- " + "End time: " + date_end)
     log_file.close()
-    sd.wait(int(duration_off))
-    
+    time.sleep(int(duration_off))
