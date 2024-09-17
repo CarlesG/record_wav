ed291d572d88addd9acadd95356ff955f8bf824d
CarlesG <carlesgallardollopis@gmail.com>
2024-09-17 11:52:52 +0200

    Create files for export virtual environment

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-
+#!/usr/bin/python3
 import subprocess
-
+import os
 # input variables for script of recording
 t_on = 5 * 60
 t_off = 5 * 60 
 fs = 44000
+print(os.getcwd())
 if __name__ == '__main__':
      subprocess.call(["python3", "record.py", str(t_on), str(t_off), str(fs)])
 
